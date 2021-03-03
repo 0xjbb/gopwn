ccbd9616f66e26ac8700e23e5533f904f772b978
j <jbork12@pm.me>
2021-03-03 10:33:37 -0800

    more code

--- gopwn.go
+++ gopwn.go
@@ -1,6 +1,7 @@
 package gopwn
 
 import (
+	"io"
 	"log"
 	"net"
 	"os/exec"
@@ -17,15 +18,30 @@ func Remote(address string) *AppProcess{
 	return NewProcess(c,c)
 }
 
-func Local(binaryName string){
+func Process(binaryName string) *AppProcess{
 	if !FileExists(binaryName){
 		log.Fatalf("File %s dooesn't exist.", binaryName)
 	}
 	cmd := exec.Command(binaryName)
 
+	stderr, err := cmd.StderrPipe()
+	if err != nil{
+		log.Fatal("Stderr error: ",err)
+	}
 
+	stdin, err := cmd.StdinPipe()
+	if err != nil{
+		log.Fatal("Stdin error: ",err)
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil{
+		log.Fatal("Stdout error: ",err)
+	}
 
+	err = cmd.Start()
+	if err != nil{
+		log.Fatal("Start error: ",err)
+	}
 
-
-
+	return NewProcess(io.MultiReader(stdout, stderr), stdin)
 }
